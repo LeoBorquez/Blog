0194329e2d03377d0828629355f2ff0623a2504d
Leo Borquez <ele.dev@protonmail.com>
2019-07-28 23:42:21 -0400

    show article view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,15 @@ class ArticlesController < ApplicationController
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article created";
-      redirect_to article_path
+      flash[:notice] = "Article created"
+      redirect_to article_path(@article)
     else
       render 'articles/new'
     end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
