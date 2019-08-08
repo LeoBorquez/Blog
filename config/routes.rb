8e8dca0bc5d69267d838644cb186f16db3e69509
Leo Borquez <ele.dev@protonmail.com>
2019-08-07 23:53:25 -0400

    user controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   get '/about', to: 'static_pages#about'
-  get '/signup', to: 'user#new'
+  get '/signup', to: 'users#new'
 
   resources :articles
 
