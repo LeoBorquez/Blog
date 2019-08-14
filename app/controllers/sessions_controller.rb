c829a202630b0ab899280542e8b3d27b9e7976a5
Leo Borquez <ele.dev@protonmail.com>
2019-08-14 09:41:50 -0400

    login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Successfuly logged in"
+    else
+      flash.now[:danger] = "There was something wrong with your login form"
+      render 'login'
+    end
   end
 
   def destroy
