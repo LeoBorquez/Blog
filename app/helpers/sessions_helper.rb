8ea97ba4a53be36d5df192c446b125e3395cae71
Leo Borquez <ele.dev@protonmail.com>
2019-08-15 21:45:56 -0400

    navigations links & sessions methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:danger] = "You need to be logged in"
+      redirect_to root_path
+    end
+  end
+
 end
