1dd3cc525fbc142df34bae7b16bded52f7094798
Leo Borquez <ele.dev@protonmail.com>
2019-08-23 22:53:30 -0400

    Add article and category association (fix)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_020359) do
+ActiveRecord::Schema.define(version: 2019_08_24_022336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
